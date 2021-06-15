2e3e97c2868eb1663e850bad0cca63a60f0e9eae
Justyna244721 <244721@student.pwr.edu.pl>
2021-06-15 16:31:29 +0200

    z logowaniem +woda i sen

--- app/src/main/java/com/example/projektaktywnosc/Sen.kt
+++ app/src/main/java/com/example/projektaktywnosc/Sen.kt
@@ -2,10 +2,84 @@ package com.example.projektaktywnosc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.view.View
+import android.widget.*
 
-class Sen : AppCompatActivity() {
+class Sen : Informacjerejestracja() {
+    private lateinit var potwierdzsen: Button
+    private lateinit var iloscsen: EditText
+    private lateinit var procentsen:TextView
+    private lateinit var senprogresbar: ProgressBar
+    private var progsen:Int=0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sen)
+
+        potwierdzsen=findViewById(R.id.potwierdzsen)
+        iloscsen=findViewById(R.id.iloscsen)
+        procentsen=findViewById(R.id.procentsen)
+        senprogresbar=findViewById(R.id.senprogresbar)
+
+        potwierdzsen?.setOnClickListener(object : View.OnClickListener{
+            override fun onClick(v: View?) {
+
+                if(wodainformacjedoguzika()){
+                    var woda= iloscsen?.text.toString().trim(){ it<= ' '}
+
+
+                    var sen1=iloscsen.text.toString().toInt()
+                    progsen+=sen1
+                    senprogresbar!!.progress=progsen
+
+
+                    if (progsen>=8){
+
+                        procentsen.setText("100%")
+                        Toast.makeText(
+                            this@Sen,
+                            resources.getString(R.string.koniecsen),
+                            Toast.LENGTH_LONG
+                        ).show()
+                    }
+                    else{
+                        var procent=(100*progsen)/8
+                        var procentstr=procent.toString()
+                        procentsen.setText(procentstr+"%")
+                    }
+
+
+
+                    Toast.makeText(
+                        this@Sen,
+                        resources.getString(R.string.dodanysen),
+                        Toast.LENGTH_SHORT
+                    ).show()
+
+
+                    iloscsen.text.clear()
+
+                }
+            }
+        })
+
+
+    }
+    private fun wodainformacjedoguzika(): Boolean {
+
+        return when{
+            TextUtils.isEmpty(iloscsen?.text.toString().trim{ it <= ' '}) -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_enter_braksnu), true)
+                false
+            }
+
+
+            else -> {
+                //showErrorSnackBar("Your details are valid",false)
+                true
+            }
+        }
+
+
     }
 }
--- app/src/main/java/com/example/projektaktywnosc/Woda.kt
+++ app/src/main/java/com/example/projektaktywnosc/Woda.kt
@@ -4,12 +4,118 @@ import android.content.Intent
 import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextUtils
+import android.text.TextWatcher
 import android.view.View
+import android.widget.*
+import androidx.appcompat.widget.ButtonBarLayout
 
-class Woda : AppCompatActivity() {
+class Woda : Informacjerejestracja() {
+    private lateinit var szklanka: Button
+    private lateinit var butelka: Button
+    private lateinit var zatwierdz:Button
+    private lateinit var iloscwody:EditText
+    private var progres:Int=0
+    private lateinit var progresbar:ProgressBar
+    private lateinit var wysprogres:TextView
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_woda)
+
+        szklanka=findViewById(R.id.szklanka)
+        butelka=findViewById(R.id.butelka)
+        zatwierdz=findViewById(R.id.zatwierdz)
+        iloscwody=findViewById(R.id.iloscwody)
+        progresbar=findViewById(R.id.wodaprogresbar)
+        wysprogres=findViewById(R.id.wysprog)
+
+        progresbar!!.max=2500
+
+        szklanka?.setOnClickListener(object : View.OnClickListener{
+            override fun onClick(v: View?) {
+                iloscwody?.append("250")
+                butelka.isEnabled=false
+                szklanka.isEnabled=false
+                zatwierdz.isEnabled=true
+
+
+            }
+        })
+        butelka?.setOnClickListener(object : View.OnClickListener{
+            override fun onClick(v: View?) {
+                iloscwody?.append("1500")
+                szklanka.isEnabled=false
+                butelka.isEnabled=false
+                zatwierdz.isEnabled=true
+            }
+        })
+        zatwierdz?.setOnClickListener(object : View.OnClickListener{
+            override fun onClick(v: View?) {
+
+                if(wodainformacjedoguzika()){
+                    var woda= iloscwody?.text.toString().trim(){ it<= ' '}
+
+
+                    var woda1=iloscwody.text.toString().toInt()
+                    progres+=woda1
+                    progresbar!!.progress=progres
+
+
+                    if (progres>=2500){
+
+                        wysprogres.setText("100%")
+                        Toast.makeText(
+                            this@Woda,
+                            resources.getString(R.string.koniecwoda),
+                            Toast.LENGTH_LONG
+                        ).show()
+                    }
+                    else{
+                        var procent=(100*progres)/2500
+                        var procentstr=procent.toString()
+                        wysprogres.setText(procentstr+"%")
+                    }
+
+
+
+                    Toast.makeText(
+                        this@Woda,
+                        resources.getString(R.string.dodanawoda),
+                        Toast.LENGTH_SHORT
+                    ).show()
+
+
+                    iloscwody.text.clear()
+                    szklanka.isEnabled=true
+                    butelka.isEnabled=true
+                }
+            }
+        })
+
+
+
+
+
+    }
+    private fun wodainformacjedoguzika(): Boolean {
+
+        return when{
+            TextUtils.isEmpty(iloscwody?.text.toString().trim{ it <= ' '}) -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_enter_brakwody), true)
+                false
+            }
+
+
+            else -> {
+                //showErrorSnackBar("Your details are valid",false)
+                true
+            }
+        }
+
+
     }
 
 
